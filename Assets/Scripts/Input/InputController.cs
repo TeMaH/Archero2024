c4ca1af5ef34aa3a8e0bf10a426e3066b3ff533f
AliaksandrParetski <aliaksandrparetski@gmail.com>
2024-07-17 19:25:20 +0300

    Joystick
    Create Joystick

--- Assets/Scripts/Input/InputController.cs
+++ Assets/Scripts/Input/InputController.cs
@@ -2,14 +2,14 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class InputController : MonoBehaviour, GameInput.IGamePlayActions
+public class InputController : MonoBehaviour, GameInput.IGamePlayActions, GameInput.IUIJoystickActions
 {
     private GameInput gameInput;
     private GameInput.GamePlayActions gamePlayActions;
 
-
-
     public static event Action<Vector2> MoveEvent;
+    public static event Action TapScreen;
+    public static event Action EndTapScreen;
 
     private void OnEnable()
     {
@@ -20,6 +20,7 @@ public class InputController : MonoBehaviour, GameInput.IGamePlayActions
             gameInput.Enable();
 
             gameInput.GamePlay.SetCallbacks(this);
+            gameInput.UIJoystick.SetCallbacks(this);
         }
     }
 
@@ -30,11 +31,24 @@ public class InputController : MonoBehaviour, GameInput.IGamePlayActions
             gameInput.Disable();
 
             gameInput.GamePlay.RemoveCallbacks(this);
+            gameInput.UIJoystick.RemoveCallbacks(this);
         }
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         MoveEvent?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnTapScreen(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Performed)
+        {
+            TapScreen?.Invoke();
+        }
+        else if(context.phase == InputActionPhase.Canceled)
+        {
+            EndTapScreen?.Invoke();
+        }
     }
 }
